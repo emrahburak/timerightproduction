e13156e48c20fb22014333b3ce9c25f5dc12ae6b
emrahburak <gemrahburak@gmail.com>
2026-01-28 16:05:55 +0300

    update: +Head.tsx added og image

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -3,8 +3,25 @@
 export function Head() {
   return (
     <>
-      <title>Time Right Production</title>
-      <meta name="description" content="Time Right Production - Profesyonel Prodüksiyon Hizmetleri" />
+      <title>Time Right Production | Creative Media & Film</title>
+      <meta name="description" content="Time Right Production - Profesyonel Prodüksiyon, Film ve Medya Hizmetleri" />
+
+      {/* Open Graph / Facebook / WhatsApp */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://timerightproduction.org" />
+      <meta property="og:title" content="Time Right Production | Profesyonel Prodüksiyon" />
+      <meta property="og:description" content="Yaratıcı çözümler ve profesyonel medya üretimi." />
+
+      {/* Görsel: Public klasöründeki tam isimle birebir aynı olmalı */}
+      <meta property="og:image" content="https://timerightproduction.org/timerightproduction-og.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
+      {/* Twitter (X) */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Time Right Production" />
+      <meta name="twitter:description" content="Yaratıcı prodüksiyon çözümleri." />
+      <meta name="twitter:image" content="https://timerightproduction.org/timerightproduction-og.png" />
     </>
   );
 }
