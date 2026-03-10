a22a5c1e52f26ae5bfa400eb070ec81bb0ef921f
emrahburak <gemrahburak@gmail.com>
2026-03-10 13:38:37 +0300

    feat: update workshops data

--- src/data/workshops.ts
+++ src/data/workshops.ts
@@ -5,25 +5,105 @@ export interface WorkshopItem {
 }
 
 export const workshops: WorkshopItem[] = [
-  { image: 'altun-egitim-01.webp', title: 'Workshop 1', description: 'Açıklama 1' },
-  { image: 'altun-egitim-02.webp', title: 'Workshop 2', description: 'Açıklama 2' },
-  { image: 'altun-egitim-03.webp', title: 'Workshop 3', description: 'Açıklama 3' },
-  { image: 'altun-egitim-04.webp', title: 'Workshop 4', description: 'Açıklama 4' },
-  { image: 'altun-egitim-05.webp', title: 'Workshop 5', description: 'Açıklama 5' },
-  { image: 'altun-egitim-06.webp', title: 'Workshop 6', description: 'Açıklama 6' },
-  { image: 'altun-egitim-07.webp', title: 'Workshop 7', description: 'Açıklama 7' },
-  { image: 'altun-egitim-08.webp', title: 'Workshop 8', description: 'Açıklama 8' },
-  { image: 'altun-egitim-09.webp', title: 'Workshop 9', description: 'Açıklama 9' },
-  { image: 'altun-egitim-10.webp', title: 'Workshop 10', description: 'Açıklama 10' },
-  { image: 'altun-egitim-11.webp', title: 'Workshop 11', description: 'Açıklama 11' },
-  { image: 'altun-egitim-12.webp', title: 'Workshop 12', description: 'Açıklama 12' },
-  { image: 'altun-egitim-13.webp', title: 'Workshop 13', description: 'Açıklama 13' },
-  { image: 'altun-egitim-14.webp', title: 'Workshop 14', description: 'Açıklama 14' },
-  { image: 'altun-egitim-15.webp', title: 'Workshop 15', description: 'Açıklama 15' },
-  { image: 'altun-egitim-16.webp', title: 'Workshop 16', description: 'Açıklama 16' },
-  { image: 'altun-egitim-17.webp', title: 'Workshop 17', description: 'Açıklama 17' },
-  { image: 'altun-egitim-18.webp', title: 'Workshop 18', description: 'Açıklama 18' },
-  { image: 'timeright-image-workshop-01.webp', title: 'Workshop 19', description: 'Açıklama 19' },
-  { image: 'timeright-image-workshop-02.webp', title: 'Workshop 20', description: 'Açıklama 20' },
-  { image: 'timeright-image-workshop-03.webp', title: 'Workshop 21', description: 'Açıklama 21' },
+  {
+    image: "altun-egitim-01.webp",
+    title: "Workshop 1",
+    description: "Açıklama 1",
+  },
+  {
+    image: "altun-egitim-02.webp",
+    title: "Workshop 2",
+    description: "Açıklama 2",
+  },
+  {
+    image: "altun-egitim-03.webp",
+    title: "Workshop 3",
+    description: "Açıklama 3",
+  },
+  {
+    image: "altun-egitim-04.webp",
+    title: "Workshop 4",
+    description: "Açıklama 4",
+  },
+  {
+    image: "altun-egitim-05.webp",
+    title: "Workshop 5",
+    description: "Açıklama 5",
+  },
+  {
+    image: "altun-egitim-06.webp",
+    title: "Workshop 6",
+    description: "Açıklama 6",
+  },
+  { image: "altun-egitim-07.webp", title: "", description: "Açıklama 7" },
+  {
+    image: "altun-egitim-08.webp",
+    title: "Workshop 8",
+    description: "Açıklama 8",
+  },
+  {
+    image: "altun-egitim-09.webp",
+    title: "Workshop 9",
+    description: "Açıklama 9",
+  },
+  {
+    image: "altun-egitim-10.webp",
+    title: "Workshop 10",
+    description: "Açıklama 10",
+  },
+  {
+    image: "altun-egitim-11.webp",
+    title: "Workshop 11",
+    description: "Açıklama 11",
+  },
+  {
+    image: "altun-egitim-12.webp",
+    title: "Workshop 12",
+    description: "Açıklama 12",
+  },
+  {
+    image: "altun-egitim-13.webp",
+    title: "Workshop 13",
+    description: "Açıklama 13",
+  },
+  {
+    image: "altun-egitim-14.webp",
+    title: "Workshop 14",
+    description: "Açıklama 14",
+  },
+  {
+    image: "altun-egitim-15.webp",
+    title: "Workshop 15",
+    description: "Açıklama 15",
+  },
+  {
+    image: "altun-egitim-16.webp",
+    title: "Workshop 16",
+    description: "Açıklama 16",
+  },
+  {
+    image: "altun-egitim-17.webp",
+    title: "Workshop 17",
+    description: "Açıklama 17",
+  },
+  {
+    image: "altun-egitim-18.webp",
+    title: "Workshop 18",
+    description: "Açıklama 18",
+  },
+  {
+    image: "timeright-image-workshop-01.webp",
+    title: "Workshop 19",
+    description: "Açıklama 19",
+  },
+  {
+    image: "timeright-image-workshop-02.webp",
+    title: "Workshop 20",
+    description: "Açıklama 20",
+  },
+  {
+    image: "timeright-image-workshop-03.webp",
+    title: "Workshop 21",
+    description: "Açıklama 21",
+  },
 ];
